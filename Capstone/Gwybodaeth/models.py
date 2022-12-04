f55d4c1804573af0ebaca8a2b48c7662c36dba34
Kamil Sidor <kamil_sidor@o2.pl>
2022-12-04 11:48:18 +0100

    Create boilerplate templates and basic file structure for the project

--- Capstone/Gwybodaeth/models.py
+++ Capstone/Gwybodaeth/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+#class User(AbstractUser):
+  
+#    def __str__(self):
+#        return f"{self.username}"
--- Capstone/Gwybodaeth/views.py
+++ Capstone/Gwybodaeth/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+import json
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.urls import reverse
+
 
 # Create your views here.
 
+def login_view(request):
+    pass
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    pass
+
+
+
+
+
+
+
 def index(request):
-    return HttpResponse("Croeso!")
+    return render(request, "gwybodaeth/index.html")

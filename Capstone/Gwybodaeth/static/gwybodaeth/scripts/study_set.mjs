8f6c6dc0bc81a2fb3be51dcc3140e0fd2e263318
Kamil Sidor <kamil_sidor@o2.pl>
2022-12-09 15:00:02 +0100

    Experimental: Set create-study-set form display to grid. TODO: make it actually look good.; Some of the HTML code has been refactored to be easier to read.

---
+++ Capstone/Gwybodaeth/static/gwybodaeth/scripts/study_set.mjs
@@ -0,0 +1,16 @@
+var StudySet = (function(){
+// Public functions
+  function testFunction() {
+    console.log("Test successfull");
+  }
+
+// Private functions
+
+
+
+return {
+  testFunction: testFunction,
+};
+})();
+
+StudySet.testFunction();

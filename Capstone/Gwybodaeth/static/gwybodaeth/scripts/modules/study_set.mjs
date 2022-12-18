4345baf38ac901901b88b5365d957a76fa72a967
Kamil Sidor <kamil_sidor@o2.pl>
2022-12-18 20:09:05 +0100

    Backup, just in case.

--- Capstone/Gwybodaeth/static/gwybodaeth/scripts/modules/study_set.mjs
+++ Capstone/Gwybodaeth/static/gwybodaeth/scripts/modules/study_set.mjs
@@ -4,6 +4,8 @@ export const StudySet = (function(){
     console.log("StudySet module loaded successfully!");
   }
 
+
+
   const createStudySet = () => {
     fetch('/create-set', {
       method: 'POST',
@@ -26,6 +28,8 @@ export const StudySet = (function(){
     return false;
   }
 
+
+
   const loadStudyTerms = () => {
     fetch(`/load/${_getStudySetID()}`)
     .then(response => response.json())
@@ -38,16 +42,75 @@ export const StudySet = (function(){
 
 // Private functions
 
+/**
+ * Creates and returns an HTML element with a single study item's term, definition, category and options.
+ * @param {Array}  termData - An array containing all the data needed to create a single study item.
+ * @param {number} termData[0] - The first element of the termData array is the ID if a study item, starting with 1
+ * @param {Object} termData[1] - An object/dictionary with 3 keys: "term", "def" (definition), "cat" (category).
+ */
 const _createStudyTerm = (termData) => {
   console.log('In createStudyTerm, termData:');
   console.log(termData);
+  console.log(termData[0]);
+  console.log(termData[1]);
+
+  const studyItem = _createStudyTermContainer(termData[0]);
+
+  Object.entries(termData[1]).forEach(([key, value]) => {
+    studyItem.append(_createStudyTermFields(key, value));
+  })
+
+  studyItem.append(_createStudyTermOptions());
+
+  console.log(studyItem);
+  return studyItem;
 }
 
+
+
+const _createStudyTermContainer = (termID) => {
+  const studyItemContainer = document.createElement('div');
+  studyItemContainer.setAttribute("id", `study-item-${termID}`);
+  studyItemContainer.classList.add("grid-container-6", "study-item");
+  
+  return studyItemContainer;
+}
+
+
+
+const _createStudyTermFields = (key, value) => {
+  console.log("Key is:" + key);
+  const studyField = document.createElement("div");
+  studyField.classList.add(`study-${key}`);
+  studyField.classList.add((key === 'cat') ? "study-set-cell-1" : "study-set-cell-2");
+  studyField.innerHTML = value;
+
+  return studyField;
+}
+
+
+
+const _createStudyTermOptions = () => {
+  // TODO: create buttons for editing and marking a term as difficult.
+  const studyOptions = document.createElement('div');
+  studyOptions.classList.add("study-set-cell-1", "study-options");
+  studyOptions.innerHTML = "TODO";
+  
+  return studyOptions;
+}
+
+
+
 const _fillStudySetViewWithTerms = (terms) => {
+  const studyItems = document.getElementById("study-items");
+
   Object.entries(terms).forEach(term => {
     _createStudyTerm(term);
+    //studyItems.append(_createStudyTerm(term));
   })
 }
+
+
 
 const _getNewStudySetData = () => {
   let   data       = {};
@@ -67,9 +130,12 @@ const _getNewStudySetData = () => {
   return data;
 }
 
+
+
 const _getStudySetID = () => {
   return window.location.pathname.slice(1)
 }
+
 
 
 //

894a0625e64fa56884f29701e58770a961db9298
Kamil Sidor <kamil_sidor@o2.pl>
2023-01-08 17:26:32 +0100

    Backup. Added some stuff for the Write module, mostly docstrings, some code tweaks and HTML skeleton for the feedback view.

--- Capstone/Gwybodaeth/static/gwybodaeth/scripts/modules/write.mjs
+++ Capstone/Gwybodaeth/static/gwybodaeth/scripts/modules/write.mjs
@@ -5,7 +5,14 @@ export const Write = function() {
   
 
 
+
+  /**
+   *  Contains references to the visible HTML elements and methods of displaying/updating them.
+   */
   const View = function() {
+    //--------------------------------------------------------------------------
+    //                     View: HTML elements references
+    //--------------------------------------------------------------------------
     const question         = document.getElementById("write-question"          );
     const pass             = document.getElementById("write-pass"              );
     const answer           = document.getElementById("write-answer-area"       );
@@ -20,14 +27,43 @@ export const Write = function() {
     const remainingCounter = document.getElementById("remaining-counter"       );
     const totalCounter     = document.querySelectorAll(".total-items"          );
 
+    //--------------------------------------------------------------------------
+    //                     View: public methods
+    //--------------------------------------------------------------------------
+    
     const initialize = (itemsCount) => {
-      _setCounters();
       _setTotalCounters(itemsCount);
       _addEventListeners();
+      update();
     }
 
     const update = () => {
+      _showCurrent();
       _setCounters();
+      _clearInput();
+    }
+
+    const getUserInput = () => {
+      if (answer.value === '') { return false; } // Invalidate empty input
+
+      return answer.value.split(/[,;/]/);
+    }
+
+    //--------------------------------------------------------------------------
+    //                     View: private methods
+    //--------------------------------------------------------------------------
+
+    const _showCurrent = () => {
+      let currentItem = Memory.currentItem();
+
+      if (!currentItem) { return alert("End reached.");}
+
+      question.innerHTML = currentItem.definitions.join(', ');
+      category.innerHTML = currentItem.category;
+    }
+
+    const _clearInput = () => {
+      answer.value = '';
     }
 
     const _addEventListeners = () => {
@@ -48,55 +84,78 @@ export const Write = function() {
     }
 
     return {
-      question  : question,
-      pass      : pass,
-      answer    : answer,
-      submit    : submit,
-      category  : category,
-      label     : label,
-      specials  : specials,
-      progress  : progress,
-      initialize: initialize,
-      update    : update
+      getUserInput: getUserInput,
+      initialize  : initialize,
+      update      : update
     }
   }();
 
 
 
+
+
+  /**
+   *  Contains data necessary for the app to work and remembers whether a term hasn't been
+   *    shown to the user or the answer given was correct/incorrect.
+   */
   const Memory = function() {
-    const remaining = [];
-    const correct   = [];
-    const incorrect = [];
+    const _remaining = [];
+    const _correct   = [];
+    const _incorrect = [];
 
+    /**
+     * Loads into memory a new StudyItem object, created from the fetched data. 
+     * @param {StudyItem} item 
+     */
     const loadItem = (item) => {
-      remaining.push(item);
+      _remaining.push(item);
     }
 
+    /**
+     * 
+     * @returns the first element from the _remaining list, which will be used to ask the user a question
+     *   and await their answer for it.
+     */
     const currentItem = () => {
-      return remaining[0];
+      return (!!_remaining[0]) ? _remaining[0] : false;
     }
 
+    /**
+     *  Marks the currentItem as being answered correctly, moving it from
+     *   the _remaining list to the _correct list.
+     */
     const markAsCorrect = () => {
-      correct.push(remaining.shift());
+      _correct.push(_remaining.shift());
     }
-
+    
+    /**
+     *  Marks the currentItem as being answered incorrectly, moving it from
+     *   the _remaining list to the _incorrect list.
+     */
     const markAsIncorrect = () => {
-      incorrect.push(remaining.shift());
+      _incorrect.push(_remaining.shift());
     }
 
+    /**
+     *  Shuffles the _remaining list randomly in order to provide the user with more challenge
+     *    and inability to associate answers with their positions in the order of questions.
+     */
     const shuffle = () => {
-      remaining.sort(() => { return Math.random() - 0.5; });
+      _remaining.sort(() => { return Math.random() - 0.5; });
     }
 
+    /**
+     *  Sorts the study items by their ID, which is assigned on study set creation.
+     */
     const sort = () => {
-      remaining.sort((first, second) => {return parseInt(first.id) - parseInt(second.id)});
+      _remaining.sort((first, second) => {return parseInt(first.id) - parseInt(second.id)});
     }
 
-    const countRemaining = () => {return remaining.length;}
+    const countRemaining = () => {return _remaining.length;}
 
-    const countCorrect   = () => {return correct  .length;}
+    const countCorrect   = () => {return _correct  .length;}
 
-    const countIncorrect = () => {return incorrect.length;}
+    const countIncorrect = () => {return _incorrect.length;}
 
     return {
       countRemaining : countRemaining,
@@ -113,6 +172,12 @@ export const Write = function() {
 
 
 
+
+  /**
+   *  Creates a single study item which will be then pushed into Memory.remaining for
+   *    storage and use. Only used to process raw fetched data from the server into
+   *    something that is more suitable for further use.
+   */
   class StudyItem {
     constructor(id, terms, definitions, category, options=null) {
       this.id          = id;
@@ -150,18 +215,12 @@ export const Write = function() {
   const _run = () => {
     Memory.shuffle();
     View  .initialize(Memory.countRemaining());
-    _showCurrent();
-  }
-
-  const _showCurrent = () => {
-    let currentItem = Memory.currentItem();
-
-    View.question.innerHTML = currentItem.definitions.join(', ');
-    View.category.innerHTML = currentItem.category;
   }
 
   const _submitAnswer = () => {
-    let input = View.answer.value.split(/[,;/]/);
+    let input   = View.getUserInput();
+    if (!input) { return false; } // Prevent accidentally sending empty input.
+
     let answers = Memory.currentItem().terms;
 
     if (_verifyAnswers(answers, input)) {
@@ -171,8 +230,7 @@ export const Write = function() {
     }
 
     View.update();
-    _showCurrent();
-    return false;
+    return false; // Prevent page reload on form submit.
   }
 
   const _verifyAnswers = (questions, answers) => {
@@ -191,8 +249,16 @@ export const Write = function() {
     return choices.map(choice => choice.trim().toLowerCase().replace(/\./g, ''));
   }
 
-  const _pass = () => {
-    console.log("IDK, next question please!");
+  /**
+   * Gives up on answering a particular question, marking it as incorrect.
+   *   Then the user will be asked the next question.
+   */
+  const _pass = () => { // TODO: this method requires repetition of View.update() and return false, which 
+                        //         are already invoked in _submitAnswer(). Find a better solution.
+    console.log("User passed.") 
+    Memory .markAsIncorrect();
+    View.update();
+    return false;
   }
 
   return {
